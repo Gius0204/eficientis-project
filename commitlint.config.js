0e497daf52e5e3917427f835279eb87bf82fb21a
Guillermo Palacios <nothicc04@gmail.com>
2025-03-24 22:00:28 -0500

    style: Add commitlint file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,28 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional']
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'header-max-length': [2, 'always', 72],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'docs',
+				'chore',
+				'style',
+				'refactor',
+				'perf',
+				'test',
+				'build',
+				'ci',
+				'revert',
+			],
+		],
+		'scope-enum': [2, 'always', [/^frontend-.+/, /^backend-.+/]],
+		'subject-empty': [2, 'never'],
+		'type-case': [2, 'always', 'lower-case'],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-case': [0],
+	},
 }
